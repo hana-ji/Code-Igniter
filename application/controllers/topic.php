fecd15eeaedca9e4b9a83aaf75a418c47aeae48e
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-08 15:44:34 +0900

    view 파일명 변경 오류

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -1,15 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Topic extends CI_Controller {
     function index(){
-//        직접적으로 적지 않고 views 폴더에 있는 부분을 가져올거임
         $this->load->view('head');
-        $this->load->view('topic');
+        $this->load->view('main');
         $this->load->view('footer');
     }
     function get($id){
         $this->load->view('head');
-        $data = array('id'=>$id);
-        $this->load->view('main', $data);
+        $this->load->view('get', array('id'=>$id));
         $this->load->view('footer');
     }
 }
--- application/views/head.php
+++ application/views/head.php
@@ -3,4 +3,3 @@
 <head>
     <meta charset="utf-8"/>
 </head>
-<body>
