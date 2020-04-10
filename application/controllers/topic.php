39994b09fad6bed3c7a4a2c7ae2ae10940964a9f
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-04-10 15:37:41 +0900

    model을 이용해서 데이터를 가져오고 보이게 ! 자꾸 에러나서 해결하고 듣고 하면서 오래걸려서 한번 더 들어봐야함

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -2,8 +2,15 @@
 class Topic extends CI_Controller {
     function index(){
         $this->load->database();
+//        model/topic_model서.php를 php 읽어서 그 안에있는 Topic_model 클래스를 로드하게 됨
+        $this->load->model('topic_model');
+//      topic_model 클래스를 가져오고, 그 가져온 클래스에 접근하기위해서는 $this->topic_model-> 이 규칙으로 적어야 접근 가능함 이렇게 하면 topic_model에 있는 get을 불러올수있
+        $data = $this->topic_model->gets();
+//        foreach ($data as $entry){
+//            var_dump($entry);
+//        } 이해 돕기위해 해본 것.
         $this->load->view('head');
-        $this->load->view('topic');
+        $this->load->view('main', array('topics'=>$data));
         $this->load->view('footer');
     }
     function get($id){
